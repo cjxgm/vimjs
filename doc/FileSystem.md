315ba4a3dcb8db1e1cdcd981a412abf5c05b185c
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2012-08-04 22:09:41 +0800

    @ rewrote fs.js

---
+++ doc/FileSystem.md
@@ -0,0 +1,11 @@
+# vim.js FileSystem Specification
+
+## Abbreviations
+
+	* FS		File System or FileSystem
+
+## Functions that FS should provide:
+
+	* open
+	* close
+
--- fs.js
+++ fs.js
@@ -1,38 +1,95 @@
 
-/* fs.js
-	Copyright (C) eXerigumo Clanjor (哆啦比猫/兰威举), 2010-2012.
-	Under GPLv2. ABSOLUTELY NO WARRANTY!
-*/
+/**** fs.js ***********************************************************
+ * File System: A UNIX style file system.
+ *
+ * Copyright (C) eXerigumo Clanjor (哆啦比猫/兰威举), 2010-2012.
+ * Under GPLv2. ABSOLUTELY NO WARRANTY!
+ **********************************************************************/
 
-function FileSystem(dev)
-{
-	this.dev = dev;
-	this.load();
-}
+/**********************************************************************
+ *
+ * FSFile
+ *
+ */
 
-FileSystem.prototype.load  = function() {
-	this.data = localStorage[this.dev];
-	if (!this.data) this.data = {};
-};
-
-FileSystem.prototype.flush = function() {
-	localStorage[this.dev] = JSON.stringify(this.data);
-};
-
-FileSystem.prototype.open = function(name) {
-	var f = new File(this, name);
-	f.data = this.data[name];
-	return f;
-};
-
-function File(fs, filename)
+function FSFile(id)
 {
 	this.fs   = fs;
-	this.name = filename;
-	this.data = '';
+	this.id   = id;
 }
 
-File.prototype.close = File.prototype.flush = function() {
-	this.fs.data[this.name] = this.data;
-	this.fs.flush();
-};
+/**********************************************************************
+ *
+ * FSDir
+ *
+ */
+
+function FSDir(fs, id)
+{
+	this.fs   = fs;
+	this.id   = id;
+}
+
+/**********************************************************************
+ *
+ * FSLocalStorage
+ *
+ */
+
+// to the local storage file system, user is useless,
+// for it must always be 'local'.
+function FSLocalStorage(user)
+{
+	this._load();
+	this.cd("/");
+}
+
+
+FSLocalStorage.prototype.ls = function()
+{
+	return this.listSubFiles(cwd.id);
+}
+
+
+FSLocalStorage.prototype.cd = function(path)
+{
+	var dir = this.getFileByPath(path);
+	if (dir instanceof FSDir) this.cwd = dir;
+	else if (dir.constructor === String) return dir;
+	else return "`" + path + "' is not a directory.";
+}
+
+
+// return a string when error occured, otherwise return an FSFile or FSDir.
+FSLocalStorage.prototype.getFileByPath = function(path)
+{
+	if (path == "/")
+		return new FSDir(this, 0);
+
+	if (path == "")
+		return this.cwd;
+
+	path = path.split("/");
+	var dir = this.cwd;
+
+	if (path[0] == '')
+		dir = new FSDir(this, 0);
+	for (var i in path) {
+		if (path[i] == '') continue;
+		if (dir.type != FS_DIR)
+			return "`" + path[i] + "' is not a directory.";
+		dir = dir.getFile(path[i]);
+	}
+	return dir;
+}
+
+
+FSLocalStorage.prototype._load = function()
+{
+	if (!localStorage['local'])
+		localStorage['local'] = JSON.stringify([
+			{ name: '', pid: 0, data: '', owner: 0 }
+		]);
+	this.fs = JSON.parse(localStorage['local']);
+}
+
