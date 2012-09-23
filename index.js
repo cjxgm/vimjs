c2d93ae64cf7731444d4df5a87421bc7f5858f8a
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2012-09-23 12:24:05 +0800

    separate javascript from index_dev.htm to index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+
+/**** index.js ********************************************************
+ * index: user login and create vimjs
+ *
+ * Copyright (C) eXerigumo Clanjor (哆啦比猫/兰威举), 2012.
+ * Under GPLv2. ABSOLUTELY NO WARRANTY!
+ **********************************************************************/
+
+var convas = new Convas("vimjs", 80, 24, 11);
+
+(function userLogin(){
+	 convas.write("Welcome to vim.js!\n", true);
+	 convas.write("You can login with \"local\"" +
+			 " for local use.\n\n", true);
+	 convas.write("login: ", true);
+	 convas.readLine(true, function(login){
+		 if (login == "local") new Vim(convas, userLogin);
+		 else {
+			 convas.write("Password: ", true);
+			 convas.readLine(false, function(password){
+				 convas.write("\nLogin incorrect\n\n", true);
+				 userLogin();
+			 });
+		 }
+	 });
+})();
+
